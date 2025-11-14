b5797011f77277be072c7689848cb3f380e85b9e
ddennis18 <osemwonken18@gmail.com>
2025-11-14 10:48:10 +0100

    styling updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,115 +4,12 @@ import Profile from './components/Profile.jsx'
 import Transaction from './components/Transaction.jsx'
 import SideBar from './components/SideBar.jsx'
 import Tabs from './components/Tabs.jsx'
+import { TxnModal } from "./components/Modals.jsx";
 import './css/style.css'
 
 let expenseCategories = ['food', 'transport', 'internet', 'rent']
 let incomeCategories = ['loan', 'friend', 'gift', 'scholarship', 'parent']
 let categories = [...expenseCategories, ...incomeCategories]
-
-function NewTxnModal ({ onClose, onFinish }) {
-  const [selectedTxnType, setSelectedTxnType] = useState('exp')
-
-  let newTxnData = {
-    type: 'exp',
-    timeStamp: new Date().toLocaleString(),
-    amount: 100,
-    description: '',
-    category: '---'
-  }
-  return (
-    <div className='fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.2)] flex justify-center items-center'>
-      <div>
-        <form
-          className='bg-white p-4 rounded-xl flex flex-col gap-4 min-w-[500px]'
-          action='xx.com'
-        >
-          <button
-            className='self-end text-3xl px-2 rounded-full hover:bg-gray-200'
-            onClick={e => {
-              e.preventDefault()
-              onClose()
-            }}
-          >
-            x
-          </button>
-          <h2 className='text-lg font-semibold'>Add A New Transaction!</h2>
-          <label htmlFor='txnType' className='font-semibold'>
-            Transaction Type
-          </label>
-          <select
-            name='txnType'
-            id='txnType'
-            className='px-2 py-1 bg-gray-200 rounded-lg w-min'
-            onChange={event => {
-              newTxnData.type = event.target.value
-              setSelectedTxnType(event.target.value)
-            }}
-          >
-            <option value='exp'>Expense</option>
-            <option value='inc'>Income</option>
-          </select>
-          <label htmlFor='amount' className='font-semibold'>
-            Amount
-          </label>
-          <input
-            type='number'
-            name='amount'
-            id='amount'
-            className='px-2 py-1 rounded-lg border-1 border-gray-500'
-            onChange={e => (newTxnData.amount = e.target.value)}
-          />
-          <label htmlFor='description' className='font-semibold'>
-            Description
-          </label>
-          <textarea
-            type='text'
-            name='decription'
-            placeholder='Enter Description...'
-            className='px-2 py-1 rounded-lg border-1 border-gray-500'
-            onChange={e => {
-              newTxnData.details = e.target.value
-            }}
-          ></textarea>
-          <label htmlFor='txnTags' className='font-semibold'>
-            Set Category
-          </label>
-          <select
-            name='txnTags'
-            id='txnTags'
-            defaultValue='---'
-            className='px-2 py-1 bg-gray-200 rounded-lg w-min'
-            onChange={events => {
-              newTxnData.category = events.target.value
-            }}
-          >
-            {(selectedTxnType === 'exp'
-              ? expenseCategories
-              : incomeCategories
-            ).map(t => {
-              return <option value={t}>{t}</option>
-            })}
-            <option value='---'>{'---'}</option>
-          </select>
-
-          {/*Submit-button */}
-          <button
-            type='submit'
-            className='text-sm font-semibold bg-green-600 w-max p-2 rounded-lg text-white self-center hover:bg-green-900'
-            onClick={e => {
-              e.preventDefault()
-              newTxnData.type = selectedTxnType
-              onFinish(newTxnData)
-              onClose()
-            }}
-          >
-            Add Transaction
-          </button>
-        </form>
-      </div>
-    </div>
-  )
-}
 
 function txnFilter (t, { type, cat }) {
   const typeCheck = type === 'all' || t.type === type
@@ -133,7 +30,7 @@ function App () {
         <Profile userName='User' />
 
         {/*Control buttons*/}
-        <div className='space-x-1 pl-4'>
+        <div className='space-x-1 pl-2'>
           <button
             className='px-2 py-1 bg-black text-white text-sm rounded-lg'
             onClick={() => setAddTxnModalIsopen(true)}
@@ -146,7 +43,7 @@ function App () {
         </div>
 
         {/*Transactions*/}
-        <div className='pl-4 space-y-2'>
+        <div className='pl-2 space-y-2'>
           <div className='space-x-4'>
             <label htmlFor='typeFilter' className='font-semibold'>
               Type:{' '}
@@ -196,9 +93,11 @@ function App () {
           </div>
         </div>
         {addTxnModalIsopen && (
-          <NewTxnModal
+          <TxnModal
             onClose={() => setAddTxnModalIsopen(false)}
             onFinish={t => setTransactions([...transactions, t])}
+            incomeCategories={incomeCategories}
+            expenseCategories={expenseCategories}
           />
         )}
       </div>
--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,6 +1,6 @@
 export default function Transaction ({ type, amount, timeStamp, category }) {
   return (
-    <div className=' border-black border-b-1 grid grid-cols-5 gap-4 text-sm items-end font-semibold hover:bg-gray-100'>
+    <div className='border-gray-500 border-b-1 grid grid-cols-5 gap-4 text-sm items-end font-semibold hover:bg-gray-100'>
       <span className={`text-${type === 'exp' ? 'red' : 'green'}-500`} >{type}</span>
       <span className=''>$ {amount}</span>
       <span>
@@ -12,7 +12,7 @@ export default function Transaction ({ type, amount, timeStamp, category }) {
       <button className='bg-gray-200 w-min px-4 py-1 hover:bg-gray-400'>
         Details
       </button>
-      <span className='font-normal'>{timeStamp}</span>
+      <span className='font-normal text-right'>{timeStamp}</span>
     </div>
   )
 }
